ebcd62f7419f1627e779b6bd66a29fdd8136638b
Christopher Tompkins <chtompkins026@gmail.com>
2018-10-16 15:54:45 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,30 @@
 # code your #valid_move? method here
 
+def turn(board)
+  puts "Please enter 1-9:"
+  #get the user input
+  user_input = gets.strip
+  #input to index
+  index = input_to_index(user_input)
+  token = current_player(board)
+  
+  #check for validation
+  if valid_move?(board,index)
+    puts 'valid move'
+    move(board, index, token)
+    display_board(board)
+   else
+    puts 'try again'
+    turn(board)
+  end
+  display_board(board)
+end
+
+
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+#CHECK FOR EMPTY SPACE
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
