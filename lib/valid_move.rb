18c3855701e1a7ba443da9ebb15d3cd0b6be9bcb
Christopher Tompkins <chtompkins026@gmail.com>
2018-10-16 15:56:56 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,26 +1,4 @@
 # code your #valid_move? method here
-def input_to_index(move)
-   index = move.to_i - 1
-   index
-end
-
-def turn(board)
-  puts "Please enter 1-9:"
-  user_input = gets.strip
-
-  index = input_to_index(user_input)
-  
-  #check for validation
-  if valid_move?(board,index)
-    puts 'valid move'
-    move(board, index, token)
-    display_board(board)
-   else
-    puts 'try again'
-    turn(board)
-  end
-  display_board(board)
-end
 
 def valid_move?(board, index)
   if index.between?(0,8) && !position_taken?(board, index)
